2cf3d3097e1457c3a726e33b3f45ea852064fd55
MFMcNolt <141301431+MFMcNolt@users.noreply.github.com>
2023-09-20 14:27:24 +1000

    added in button function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,18 @@
 var todayDate = dayjs().format('dddd, MMMM D YYYY');
 $("#currentDay").html(todayDate);
 
+//
+$(document).ready(function () {
+  // saveBtn click listener 
+  $(".saveBtn").on("click", function () {
+      // Get nearby values of the description in JQuery
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
 
-$(function () {
+      // Save text in local storage
+      localStorage.setItem(time, text);
+  })
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
